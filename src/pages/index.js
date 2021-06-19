5c3a590f184e5d79a1581b3bf764fb004bef26b6
nguyen tran <nguyen.tran6698@gmail.com>
2021-06-19 09:55:08 -0500

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Cart from "./CartPage";
+import SingleProduct from "./SingleProductPage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import AuthWrapper from "./AuthWrapper";
+export {
+  Home,
+  About,
+  Error,
+  Checkout,
+  Cart,
+  SingleProduct,
+  PrivateRoute,
+  Products,
+  AuthWrapper,
+};
